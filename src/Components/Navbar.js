3bfe4e58dde90a5ba562b252505382d3fde434f7
giacomo9999 <gmarchesi888@gmail.com>
2021-02-23 16:03:35 -0500

    This tutorial is kind of a mess.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,41 @@
 import React, * as react from "react";
+import { Link, Route, Switch } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/paper";
 import Grid from "@material-ui/core/grid";
+
 import Dropdown from "./Dropdown";
+
+import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
   const [listOpen, setListOpen] = react.useState(false);
   return (
-    <ul>
-      <li
-        onClick={() => {
-          setListOpen(!listOpen);
-          console.log("List Open: ", listOpen);
-        }}
-      >
-        Projects
-      </li>
-      <div>{listOpen ? <Dropdown /> : null}</div>
-      <li onClick={() => setListOpen(false)}>Blog</li>
-      <li onClick={() => setListOpen(false)}>Skills</li>
-      <li onClick={() => setListOpen(false)}>Story</li>
-    </ul>
+    <nav className={styles.navbar}>
+      <Link to="/" className={styles.logo}>
+        <h3>LOGO</h3>
+      </Link>
+      <ul>
+        <li
+          onClick={() => {
+            setListOpen(!listOpen);
+            console.log("List Open: ", listOpen);
+          }}
+        >
+          Projects
+        </li>
+        <div className={styles.dropdown}>{listOpen ? <Dropdown /> : null}</div>
+        <li onClick={() => setListOpen(false)}>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li onClick={() => setListOpen(false)}>
+          <Link to="/skills">Skills</Link>
+        </li>
+        <li onClick={() => setListOpen(false)}>
+          <Link to="/story">Story</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
